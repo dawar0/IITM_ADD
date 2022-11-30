f71afd14055876d5002e4068ee6d992edee4244f
Dawar <dawarfarooq0@gmail.com>
2022-11-30 12:32:13 +0530

    added logic

---
+++ app.py
@@ -0,0 +1,8 @@
+# take two numbers from user and add them
+# and print the result
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Add two numbers
+sum = float(num1) + float(num2)
